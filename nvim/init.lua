12f840afce491da566fcc3d37ba350e8de80b54e
Jacky Yeh <jacky.yeh@memryx.com>
2026-05-15 21:06:30 -0400

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,8 @@
 require "options"
 require "keymaps"
 
+vim.opt.mouse = 'a' -- Enable mouse mode in all modes
+
 -- setup lazy nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
