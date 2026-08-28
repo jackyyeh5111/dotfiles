3149e11d660bf2cab2e6fb96f30e573e5bb19e4d
Jacky Yeh <jacky.yeh@memryx.com>
2026-08-28 12:18:14 -0400

    Add lualine plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -529,10 +529,39 @@ local commentor =
     end
 }
 
+local lualine = {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "catppuccin-mocha",
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+        })
+    end,
+}
+
 return {
     commentor,
     devicons,
     colortheme,
+    lualine,
     telescope,
     treesitter,
     lspconfig,
